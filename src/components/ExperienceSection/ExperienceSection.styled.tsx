cda7c64b0fd831cf603342689129ddcdfced5ee1
Septy28 <calin.septimiu28@gmail.com>
2024-05-17 19:00:33 +0100

    Changed refs to use "forwardRef" for a correct approach; Added animation on scroll using intersection observers;

--- src/components/ExperienceSection/ExperienceSection.styled.tsx
+++ src/components/ExperienceSection/ExperienceSection.styled.tsx
@@ -1,14 +1,22 @@
 "use client";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-export const Container = styled.div`
+// Styled component with conditional animation
+export const Container = styled.div<{ $inView: boolean }>`
     display: flex;
     flex-direction: column;
     align-items: center;
     padding: ${({ theme }) => theme.padding.medium};
-    background: ${({ theme }) => theme.colors.surface.primary_shade.dark_1};
     box-shadow: ${({ theme }) =>
         `inset 10px 10px 10px -1px ${theme.colors.surface.primary_shade.dark_3}, inset -10px -10px 10px -1px ${theme.colors.surface.primary_shade.dark_1}`};
+    background: ${({ theme }) => theme.colors.surface.primary_shade.dark_1};
+    transition: box-shadow 0.5s, background 0.5s;
+    ${({ $inView }) =>
+        $inView &&
+        css`
+            box-shadow: none;
+            background: ${({ theme }) => theme.colors.surface.primary};
+        `}
 `;
 
 export const ScrollToExperienceHeader = styled.h1`
