2eb9c201744a1ec7ac474db6f4ed94029df474a7
Septy28 <calin.septimiu28@gmail.com>
2024-06-03 21:24:00 +0100

    Small error fixing in error messages

--- src/components/Sections/FractalsGeneratorSections/FractalsSection/ParametersMenu/InputError.tsx
+++ src/components/Sections/FractalsGeneratorSections/FractalsSection/ParametersMenu/InputError.tsx
@@ -4,51 +4,26 @@ import * as InputValidation from "@/utils/parametersValidation";
 import { useParameters } from "@/components/Sections/FractalsGeneratorSections/FractalsSection/ParametersProvider/ParametersProvider";
 
 export default function InputError() {
-    const { typedColorModeParameters, typedParameters, parameters } =
-        useParameters();
+    const { typedColorModeParameters, typedParameters, parameters } = useParameters();
     return (
         <React.Fragment>
-            {!InputValidation.isColorIntensityValid(
-                typedColorModeParameters.colorIntensity
-            ) && (
-                <ErrorText>
-                    Color intensity must be a number different from 0
-                </ErrorText>
+            {!InputValidation.isColorIntensityValid(typedColorModeParameters.colorIntensity) && (
+                <ErrorText>Color intensity must be a number different from 0</ErrorText>
             )}
-            {!InputValidation.isRedWeightValid(
-                typedColorModeParameters.rgbWeights.r
-            ) && (
-                <ErrorText>
-                    Red weight must be a number greater than or equal than 0
-                </ErrorText>
+            {!InputValidation.isRedWeightValid(typedColorModeParameters.rgbWeights.r) && (
+                <ErrorText>Red weight must be a number greater than or equal to 0</ErrorText>
             )}
-            {!InputValidation.isGreenWeightValid(
-                typedColorModeParameters.rgbWeights.g
-            ) && (
-                <ErrorText>
-                    Green weight must be a number greater than or equal than 0
-                </ErrorText>
+            {!InputValidation.isGreenWeightValid(typedColorModeParameters.rgbWeights.g) && (
+                <ErrorText>Green weight must be a number greater than or equal to 0</ErrorText>
             )}
-            {!InputValidation.isBlueWeightValid(
-                typedColorModeParameters.rgbWeights.b
-            ) && (
-                <ErrorText>
-                    Blue weight must be a number greater than or equal to 0
-                </ErrorText>
+            {!InputValidation.isBlueWeightValid(typedColorModeParameters.rgbWeights.b) && (
+                <ErrorText>Blue weight must be a number greater than or equal to 0</ErrorText>
             )}
             {!InputValidation.isColorsNumberValid(
                 typedColorModeParameters.numberOfRandomColors
-            ) && (
-                <ErrorText>
-                    Number of colors must be a number greater than 0
-                </ErrorText>
-            )}
-            {!InputValidation.isMaxIterationsValid(
-                typedParameters.maxIterations
-            ) && (
-                <ErrorText>
-                    Max iterations must be a number greater than 0
-                </ErrorText>
+            ) && <ErrorText>Number of colors must be a number greater than 0</ErrorText>}
+            {!InputValidation.isMaxIterationsValid(typedParameters.maxIterations) && (
+                <ErrorText>Max iterations must be a number greater than 0</ErrorText>
             )}
             {!InputValidation.isWidthValid(typedParameters.width) && (
                 <ErrorText>Width must be a number greater than 3</ErrorText>
