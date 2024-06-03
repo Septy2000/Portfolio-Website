14a5a38eb25aac40f40f5067afc48464a89ceae8
Septy28 <calin.septimiu28@gmail.com>
2024-06-03 20:58:36 +0100

    Trying to fix duplicate folders error

--- src/components/wrappers/PlainLinkWrapper/PlainLinkWrapper.styled.tsx
+++ src/components/wrappers/PlainLinkWrapper/PlainLinkWrapper.styled.tsx
@@ -3,8 +3,6 @@ import Link from "next/link";
 import styled from "styled-components";
 
 export const PlainLinkWrapper = styled(Link)`
-    /* color: inherit;
-    text-decoration: none; */
     display: inline-block;
     width: 100%;
 `;
