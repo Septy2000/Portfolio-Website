1f36ff65a3a4bf85a6cbfe26ba4dcd182fc7de58
Septy28 <calin.septimiu28@gmail.com>
2024-06-03 20:59:09 +0100

    Deleted Files

--- src/components/Wrappers/PlainLinkWrapper/PlainLinkWrapper.styled.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import Link from "next/link";
-import styled from "styled-components";
-
-export const PlainLinkWrapper = styled(Link)`
-    display: inline-block;
-    width: 100%;
-`;
