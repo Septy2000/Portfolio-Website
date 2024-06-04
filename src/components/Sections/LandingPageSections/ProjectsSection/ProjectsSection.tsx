1c9cb7cce8093d7a90ca83aadc12c58415ca0a3a
Septy28 <calin.septimiu28@gmail.com>
2024-06-04 19:43:30 +0100

    Removed a test project from prod;

--- src/components/Sections/LandingPageSections/ProjectsSection/ProjectsSection.tsx
+++ src/components/Sections/LandingPageSections/ProjectsSection/ProjectsSection.tsx
@@ -30,13 +30,13 @@ const ProjectsSection = forwardRef<HTMLDivElement | null>((props, projectsRef) =
                 "https://github.com/Septy2000/Portfolio-Website/tree/main/src/components/Sections/FractalsGeneratorSections/FractalsSection",
         },
 
-        {
-            title: "A* Algorithm Visualiser",
-            imagePath: a_star_image_src,
-            description: "Visualise and experiment with the A* algorithm",
-            slug: undefined,
-            code_url: "https://github.com/Septy2000/Portfolio-Website",
-        },
+        // {
+        //     title: "A* Algorithm Visualiser",
+        //     imagePath: a_star_image_src,
+        //     description: "Visualise and experiment with the A* algorithm",
+        //     slug: undefined,
+        //     code_url: "https://github.com/Septy2000/Portfolio-Website",
+        // },
     ];
     const [selectedProject, setSelectedProject] = useState<Project>(projects[0]);
 
